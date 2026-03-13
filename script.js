309905ff7d14f160e960df5536a6b8afbc9cc0a8
Raj6773 <rajkishore6773@gmail.com>
2026-03-14 01:15:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function openProduct(id){
 
-window.location.href="product.html?id="+id
+window.location.href="product.html"
 
 }
+
+function changeImage(img){
+
+document.getElementById("mainImage").src=img.src
+
+}
+
+setInterval(function(){
+
+document.querySelectorAll(".slider").forEach(slider=>{
+
+let slides=slider.querySelectorAll(".slide")
+
+let active=slider.querySelector(".active")
+
+let index=[...slides].indexOf(active)
+
+active.classList.remove("active")
+
+slides[(index+1)%slides.length].classList.add("active")
+
+})
+
+},3000)
